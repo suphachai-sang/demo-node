df44d7812a65281c455353ad6d3935e702dd9568
Suphachai Sang <31473954+suphachai-sang@users.noreply.github.com>
2023-08-21 17:02:20 +0700

    first

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var cors = require('cors')
+require('dotenv').config()
+var app = express()
+app.use(cors())
+
+const mysql = require('mysql2')
+const pool = mysql.createPool({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE,
+})
+
+
+app.get('/hello-world', function (req, res, next) {
+  res.json({msg: 'hello world'})
+})
+
+app.get('/city',function(req, res, next){
+    pool.query("SELECT * FROM world.city",function(err,rows,field){
+        res.json(rows)
+    })
+})
+
+app.listen(5000, function () {
+  console.log('CORS-enabled web server listening on port 5000')
+})
+
